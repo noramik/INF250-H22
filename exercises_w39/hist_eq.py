ed467f3a5039f06bc225167278f7a99b727ec173
Nora Mikarlsen <nora.mikarlsen@nmbu.no>
2022-10-12 12:25:09 +0200

    Add function for histogram equalisation as a module

---
+++ exercises_w39/hist_eq.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def hist_equalisation(image):
+    # Histogram equalisation
+
+    histogram = np.zeros(256)
+    shape = np.shape(image)
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            pixval = int(image[i, j])
+            histogram[pixval] += 1
+
+    cumhist = np.zeros(256)
+    cumhist[0] = histogram[0]
+    for i in range(255):
+        cumhist[i + 1] = cumhist[i] + histogram[i + 1]
+
+    K = 256
+    M = shape[0]
+    N = shape[1]
+    image_he = np.zeros(image.shape)
+    for i in range(M):
+        for j in range(N):
+            a = int(image[i, j])
+            b = cumhist[a] * (K - 1) / M * N
+            image_he[i, j] = b
