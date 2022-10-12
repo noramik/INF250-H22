7973e98861d53253d779359f718a10210fa35cba
Nora Mikarlsen <nora.mikarlsen@nmbu.no>
2022-10-12 12:36:38 +0200

    Add more plots

--- exercises_w39/exercises_w39.py
+++ exercises_w39/exercises_w39.py
@@ -1,16 +1,35 @@
 from skimage import io
 import numpy as np
 import matplotlib.pyplot as plt
+from hist_eq import hist_equalisation
+from skimage.filters import gaussian, median
 
+# Plotting original image
 filepath = '../images/UrStoy.tif'
 image = io.imread(filepath)
-fig, axes = plt.subplots(nrows=3, ncols=2)
+fig, axes = plt.subplots(nrows=2, ncols=3)
 ax = axes.ravel()
 ax[0].imshow(image, cmap='gray')
 ax[0].set_title('Original image')
 
-ax[0].imshow(hist_im, cmap='gray')
+#Histogram equalisation
+hist_im = hist_equalisation(image)
+ax[1].imshow(hist_im, cmap='gray')
 ax[1].set_title('Histogram equalisation')
+
+# Gaussian filter
+gauss_im = gaussian(image)
+ax[2].imshow(gauss_im, cmap='gray')
+ax[2].set_title('Gaussian filter')
+
+
+
+# Mean filter
+
+# Median filter
+med_im = median(image)
+ax[4].imshow(med_im, cmap='gray')
+ax[4].set_title('Median filter')
 
 
 plt.show()
